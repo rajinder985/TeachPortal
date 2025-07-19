3b2beceba5835de27187d03d62585331041bdf7f
Monalisa <monalisakargwal@gmail.com>
2025-07-20 01:21:09 +1000

    Code Cleanup : Removing unused namespace
    Code Cleanup : Removing unused namespace

--- backend/TeacherPortal.Data/Repositories/Interfaces/IStudentRepository.cs
+++ backend/TeacherPortal.Data/Repositories/Interfaces/IStudentRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TeacherPortal.Data.DTOs;
+﻿using TeacherPortal.Data.DTOs;
 using TeacherPortal.Data.Models;
 
 namespace TeacherPortal.Data.Repositories.Interfaces
--- backend/TeacherPortal.Services/Interfaces/IStudentService.cs
+++ backend/TeacherPortal.Services/Interfaces/IStudentService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TeacherPortal.Data.DTOs;
+﻿using TeacherPortal.Data.DTOs;
 
 namespace TeacherPortal.Services.Interfaces
 {
